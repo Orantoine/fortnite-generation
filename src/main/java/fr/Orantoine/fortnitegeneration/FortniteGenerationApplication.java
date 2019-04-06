2b1c09b3ef8b213f27e60b3362e9dc1818fbddfe
Antoine Godeau <antoine.godeau@sfr.fr>
2019-04-06 10:51:32 +0200

    Initialisation du projet

---
+++ src/main/java/fr/Orantoine/fortnitegeneration/FortniteGenerationApplication.java
@@ -0,0 +1,13 @@
+package fr.Orantoine.fortnitegeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortniteGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortniteGenerationApplication.class, args);
+	}
+
+}
