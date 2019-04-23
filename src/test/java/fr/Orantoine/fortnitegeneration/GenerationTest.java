793c5ade8ada81403b44485669edc69ef8d0704e
Antoine <antoine.godeau@sfr.fr>
2019-04-23 15:29:13 +0200

    Remise du test classique

--- src/test/java/fr/Orantoine/fortnitegeneration/GenerationTest.java
+++ src/test/java/fr/Orantoine/fortnitegeneration/GenerationTest.java
@@ -8,6 +8,5 @@ public class GenerationTest extends atos.amf.fft.BaseTestCase {
     @Test
     public void healthCheck(){
         System.out.println("Health Check");
-        RestAssured.given().when().log().all().get("health").then().assertThat().statusCode(200);
     }
 }
